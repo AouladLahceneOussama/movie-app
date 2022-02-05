43786e0693df830c9d072ede4d5ebfba5d6c9dc7
Oussama AOULAD LAHCENE <oussama.tet.9@gmail.com>
2022-02-05 15:27:48 +0100

    movie-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
+import {BrowserRouter as Router, Switch ,Route} from 'react-router-dom';
 import './App.css';
+import Nav from './components/Nav';
+import MainContent from './components/mainContent';
+import MovieDetail from './components/movieDetail';
 
 function App() {
+
+  const [watchList,setWatchList] = useState([]);
+  
+  useEffect(() => {
+        getWatchListLocal();
+  }, []);
+
+  function getWatchListLocal(){
+      if (localStorage.getItem('watchList') !== null) {
+          setWatchList(JSON.parse(localStorage.getItem('watchList')));
+      }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <Router>
+          <Nav watchList={watchList} setWatchList={setWatchList} />
+            <Switch>
+              <Route path="/movie/:id" component={MovieDetail} />
+              <MainContent watchList={watchList} setWatchList={setWatchList} />
+            </Switch>
+          </Router>
+      </div>
   );
 }
 
